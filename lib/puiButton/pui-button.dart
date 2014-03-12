cfccb9e9a4331963b3cfa1a7aa37e44f7d4629f8
stephanrauh <stephan.rauh@gmx.net>
2014-03-12 20:29:56 +0100

    fixed layout bugs in pui-button

--- lib/puiButton/pui-button.dart
+++ lib/puiButton/pui-button.dart
@@ -3,9 +3,11 @@ library puiButton;
 import 'dart:html';
 import 'package:angular/angular.dart';
 
-@NgComponent(selector: 'pui-button', templateUrl:
-    'packages/angularprime_dart/puiButton/pui-button.html', cssUrl:
-    'packages/angularprime_dart/puiButton/pui-button.css', publishAs: 'cmp')
+@NgComponent(
+    selector: 'pui-button', 
+    templateUrl: 'packages/angularprime_dart/puiButton/pui-button.html', 
+    cssUrl: 'packages/angularprime_dart/puiButton/pui-button.css', 
+    publishAs: 'cmp')
 class PuiButtonComponent extends NgShadowRootAware {
 
   @NgAttr("onClick")
@@ -16,24 +18,59 @@ class PuiButtonComponent extends NgShadowRootAware {
   String icon;
   @NgAttr("iconPos")
   String iconPos;
+  
+  @NgAttr("disabled")
+  String disabled;
 
   /** The scope is needed to add watches. */
   Scope scope;
 
+  /** The <puiButton> as defined in the user's source code */
   Element puiButton;
 
+  /** The button component within the shadow tree */
   ButtonElement button;
 
   PuiButtonComponent(this.scope, this.puiButton) {}
-
+  
+  /** We add the optional icon to the button during the initialization of the shadow DOM tree. */
   void onShadowRoot(ShadowRoot shadowRoot) {
     shadowRoot.applyAuthorStyles = true;
     button = shadowRoot.querySelector('button');
+    
+    if (disabled!=null)
+    {
+      button.classes.add("ui-state-disabled"); 
+    }
+    else
+    {
+      button.onMouseEnter.listen((event) => button.classes.add('ui-state-hover'));
+      button.onMouseLeave.listen((event) => button.classes.remove('ui-state-hover'));
+    }
 
-    //    button.onMouseEnter.listen((event) => button.classes.add('ui-state-hover'));
-    //    button.onMouseLeave.listen((event) => button.classes.remove('ui-state-hover'));
+    if (icon == null) {
+      button.classes.add('pui-button-text-only');
+    } else {
+      drawIcon();
+    }
+    
   }
 
+  /** adds the icon as a span element */
+  void drawIcon() {
+    
+    if ("right" == iconPos) {
+      button.classes.add('pui-button-text-icon-right');
+    } else {
+      button.classes.add('pui-button-text-icon-left');
+    }
+    SpanElement iconSpan = new SpanElement();
+    iconSpan.classes.add(iconSpanClass());
+    button.children.insert(0, iconSpan);
+        
+  }
+
+  /** return the CSS class defining the icon position */
   String iconPosClass() {
     if (null==icon) return "pui-button-text-only";
     String c = "pui-button-icon-left";
@@ -43,9 +80,15 @@ class PuiButtonComponent extends NgShadowRootAware {
     return c;
   }
   
+  /** returns the CSS classes defining the icon */
   String iconSpanClass()
   {
-    String c = "pui-button-icon-left ui-icon ";
+    String c = "pui-button-icon-left";
+    if ("right" == iconPos) {
+      c = "pui-button-icon-right";
+    }
+    
+    c+= " ui-icon ";
     if (icon.startsWith("ui-icon-"))
       c+=icon;
     else
@@ -53,10 +96,4 @@ class PuiButtonComponent extends NgShadowRootAware {
     print(c);
     return c;
   }
-  
-  bool showIcon()
-  {
-    return null != icon;
-  }
-
 }
